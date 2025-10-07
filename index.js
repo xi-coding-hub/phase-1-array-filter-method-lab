9b7945ec927ffef6a444e0fbd7f0d692d6ce6eb8
el-sam <samurai.h@yahoo.com>
2025-10-07 15:32:33 +0800

    all compte

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(driversNames, str){
+    
+    let driversSelected = driversNames.filter(driverName => driverName.toUpperCase() === str.toUpperCase())
+    return driversSelected
+}
+
+function fuzzyMatch(driverNames,str) {
+    let driversSelected = driverNames.filter(driverName => driverName.substring(0,str.length)===str)
+    return driversSelected
+
+}
+
+function matchName(drivers, name) {
+    let driversSelected = drivers.filter(driver => driver.name === name)
+    return driversSelected
+}
